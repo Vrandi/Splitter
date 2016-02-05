e12ee6ab2a77be912e5be987737a44c88cd6a8be
Vrabély Andrea <vrabely.andrea@gmail.com>
2016-02-05 14:38:06 +0100

    handle connect error

--- events.js
+++ events.js
@@ -12,8 +12,8 @@ function getAll(cb) {
 function addEntry(params, callback) {
   sendQuery(sql`
     INSERT INTO event (total_cost, event_name, members, date)
-    VALUES (${params.total_cost}, ${params.event_name}, ${params.members}, ${params.date})
-    RETURNING event_id, event_name, total_cost, members, date`,
+    VALUES (${params['total_cost']}, ${params['event_name']}, ${params['members']}, ${params['date']})
+    RETURNING event_id, total_cost, event_name, members, date`,
     callback);
 }
 
@@ -24,13 +24,16 @@ function removeEntry(id, callback) {
     callback);
 }
 
-
 function sendQuery(query, callback) {
-  pg.connect(databaseUrl, function(err, client, done){
-    client.query(query, function(err, result) {
-      done();
-      callback(err, result);
-    });
+  pg.connect(databaseUrl, function(connectErr, client, done){
+    if (connectErr) {
+      callback(connectErr);
+    } else {
+      client.query(query, function(err, result) {
+        done();
+        callback(err, result);
+      });
+    }
   });
 }
 
